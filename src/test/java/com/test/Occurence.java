01af8a4d039976daff2fb164d94557304354550d
UdayaMarvel <udayamarvel@gmail.com>
2025-04-25 08:38:44 +0530

    Occurence.class

---
+++ src/test/java/com/test/Occurence.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Occurence {
+public static void main(String[] args) {
+	String s = "Udaya";
+	Map<Character, Integer> mp = new HashMap();
+	for(int i =0; i <s.length();i++) {
+		char ch = s.charAt(i);
+		
+	}
+}
+}
