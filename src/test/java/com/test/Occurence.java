88e50a313d69ba369a66786f4d1343ee09dbea35
UdayaMarvel <udayamarvel@gmail.com>
2025-04-25 08:49:32 +0530

    code completed

--- src/test/java/com/test/Occurence.java
+++ src/test/java/com/test/Occurence.java
@@ -9,7 +9,13 @@ public static void main(String[] args) {
 	Map<Character, Integer> mp = new HashMap();
 	for(int i =0; i <s.length();i++) {
 		char ch = s.charAt(i);
-		
+		if(mp.containsKey(ch)) {
+			mp.put(ch, mp.get(ch)+1);
+		}
+		else {
+			mp.put(ch,1);
+		}
 	}
+	System.out.println(mp);
 }
 }
