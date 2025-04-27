ccbba96b1d8db4b88bdead847f96f045d251eec5
bharath-s-k-1999 <bharathkuppusamy1999@gmail.com>
2025-04-27 23:42:34 +0530

    palindrome.class

---
+++ src/test/java/bharath/palindrome.java
@@ -0,0 +1,26 @@
+package bharath;
+
+public class palindrome {
+	
+	public static void main(String[]args) {
+
+String a = "malayalam";
+	
+	char []b=a.toCharArray();
+	
+	for (int i=b.length-1;i>=0;i--) {
+		
+		System.out.print(b[i]);
+
+	}		
+	System.out.println(" ");
+	
+	String d= new String (b);
+
+	if (a.equals(d)) {
+		System.out.println("palindrom");
+	}
+	else {System.out.println("not a palindrom");}
+}
+
+}
